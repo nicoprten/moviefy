a5073d222383f114fccd8eedb8ce44f11e390819
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2022-10-09 02:12:07 -0300

    Finishim slider and other detials

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import LastMovies from './components/LastMovies';
+import {Routes, Route} from 'react-router-dom';
+
+// COMPONENTS
+import { LastMovies } from './components/LastMovies';
+import { LogIn } from './components/LogIn';
+import { NavBar } from './components/NavBar';
+
 import 'tailwindcss/tailwind.css';
 
 export default function App(){
 
     return(
-        <div className='w-screen mx-auto'>
-            <h1 className='text-3xl font-bold'>Moviefy</h1>
-            <LastMovies />
+        <div className='w-100vw font-kanit'>
+            <NavBar />
+            <Routes>
+                <Route path={'/'} element={<LastMovies />} />
+                <Route path={'/login'} element={<LogIn />} />
+            </Routes>
         </div>
     )
 }
--- src/components/LastMovies.tsx
+++ src/components/LastMovies.tsx
@@ -1,7 +1,12 @@
 import React, { MouseEvent, useRef } from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
+// COMPONENTS
+import MovieCard from './MovieCard';
+
+// ICONS
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
@@ -9,56 +14,64 @@ interface Movie{
     id: number,
     title: string,
     poster_path: string,
-    release_date: string
+    release_date: string,
+    vote_average: number
 }
 
-export default function LastMovies(){
+export const LastMovies = () => {
 
     const [lastMovies, setLastMovies] = useState<Array<Movie>>([]);
+    console.log(lastMovies)
 
     const sliderRef = useRef<HTMLDivElement>(null);
-    console.log(lastMovies)
+
+    const date = new Date();
+    let releaseDateNow = `${date.getFullYear()}-${parseDate(date.getMonth() + 1)}-${parseDate(date.getDate())}`;
+    let releaseDateWeek = `${date.getFullYear()}-${parseDate(date.getMonth() + 1)}-${parseDate(date.getDate() + 7)}`;
+
+    // Example: 8 => return 08
+    function parseDate(date: number) {
+        const newDate = date < 10 ? `0${date}` : date;
+        return newDate;
+    }
+
     useEffect(() => {
-        axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=5a124b627856216bf209486ade0bc869')
+        axios.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=${releaseDateNow}&primary_release_date.lte=${releaseDateWeek}&api_key=5a124b627856216bf209486ade0bc869`)
         .then(r => r.data)
         .then(d => setLastMovies(d.results))
     }, [])
 
     const handleBackSlide = (e: MouseEvent<HTMLButtonElement>) => {
-        // sliderRef.current!.scroll(500, 500);
-        sliderRef.current!.scrollLeft -= 815;
-        console.log(sliderRef.current!.scrollLeft)
-        console.log(sliderRef)
+        sliderRef.current!.scrollLeft -= sliderRef.current!.clientWidth;
     }
     
     const handleNextSlide = (e: MouseEvent<HTMLButtonElement>) => {
-        sliderRef.current!.scrollLeft += 815;
-        console.log(sliderRef.current!.scrollLeft)
-        console.log(sliderRef)
+        sliderRef.current!.scrollLeft += sliderRef.current!.clientWidth;
     }
 
     return(
-        <>
-            <h2 className='text-blue text-2xl font-medium'>Last movies added</h2>
-            <div className='relative px-8'>
-                <button onClick={handleBackSlide}>
-                    <ArrowBackIosNewIcon className='text-blue absolute top-1/2 left-0 z-0 hover:cursor-pointer'/>
-                </button>
-                <div className='flex h-auto py-4 overflow-hidden scroll scroll-smooth' ref={sliderRef}>
-                    {lastMovies?.map(m => 
-                        <div className='bg-gray-dark shrink-0 mx-2 rounded-xl hover:cursor-pointer hover:scale-105 duration-300 pb-2 z-1' key={m.id}>
-                            <img className='rounded-xl rounded-b-none h-[220px] object-cover' src={`https://image.tmdb.org/t/p/original${m.poster_path}`} alt={m.title}/>
-                            <div className='mx-auto mt-2 w-32'>
-                                <p className='text-blue font-medium'>{m.title.substring(0, 15)}</p>
-                                <p className='text-blue text-sm'>{m.release_date}</p>
-                            </div>
+        <div className="w-80vw mx-auto my-4">
+            {lastMovies.length > 0 ? 
+                <div>
+                    <h2 className='text-blue text-2xl font-bold px-8'>Movies next week</h2>
+                    <p className='text-white px-8'>Add them to your <Link className='text-blue hover:text-white' to='/watchlist'>watchlist</Link></p>
+                    <div className='relative px-8'>
+                        <button onClick={handleBackSlide}>
+                            <ArrowBackIosNewIcon className='text-blue absolute top-1/2 left-0 z-0 hover:cursor-pointer'/>
+                        </button>
+                        <div className='flex h-auto py-4 overflow-x-auto scroll scroll-smooth' ref={sliderRef}>
+                            {lastMovies?.map(m => 
+                                m.poster_path && <MovieCard {...m} key={m.id}/>
+                            )}
                         </div>
-                    )}
+                        <button onClick={handleNextSlide}>
+                            <ArrowForwardIosIcon className='text-blue absolute top-1/2 right-0 z-0 hover:cursor-pointer'/>
+                        </button>
+                    </div>
                 </div>
-                <button onClick={handleNextSlide}>
-                    <ArrowForwardIosIcon className='text-blue absolute top-1/2 right-0 z-0 hover:cursor-pointer'/>
-                </button>
-            </div>
-        </>
+            :
+                <p className='text-blue'>Waiting for movies</p>
+            }
+        </div>
     )
 }
